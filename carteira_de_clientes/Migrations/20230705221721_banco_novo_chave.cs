0660fc4d1f23fa623ff3ec2ef9f6a68dc75af02b
vitor0114 <vitorgustav0114@gmail.com>
2023-07-05 19:22:50 -0300

    alteracoes

---
+++ carteira_de_clientes/Migrations/20230705221721_banco_novo_chave.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace carteira_de_clientes.Migrations
+{
+    /// <inheritdoc />
+    public partial class banco_novo_chave : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
